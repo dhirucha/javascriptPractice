16872f90cf84103b90fb0f36962047aac473024c
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2023-12-20 16:35:24 +0530

    Learned JS basics

---
+++ class_01/basics.js
@@ -0,0 +1,52 @@
+//  console.log("Hello! Dheeraj");
+//  console.log('I love js');
+
+// name = "Tony"
+// console.log(name);
+
+// x = null;
+
+// radius = 14;
+
+// fullName = 'Chaubey';
+// firstName = 'Dheeraj';
+
+// console.log('firstName');
+// console.log('fullName');
+
+// let name = 'tony starc'
+// console.log(name);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// const student = {
+//     fullName: "rahul",
+//     age:18,
+//     cgpa:8.29
+// };
+
+// student["age"] = student['age'] + 1;
+
+// console.log(student['age']);
+
+// const product = {
+
+//     name: "Ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270
+// }
+
+// console.log(product);
+
+const profile = {
+    userName: 'Sharaddha khapra',
+    isFollow: false,
+    followers: 123,
+    following: 23
+
+
+};
+
+console.log(profile);
