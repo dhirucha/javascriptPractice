f8fa846e5caa07fcff385341f49984bcd0b5d5b6
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2023-12-31 15:20:40 +0530

    classes and objects in js

---
+++ class_10/script.js
@@ -0,0 +1,60 @@
+//classes and objects
+
+// const student = {
+//     name: 'dj',
+//     marks:9.5,
+//     printMarks: function () {
+//         console.log('marks = ',this.marks);
+//     },
+// }
+
+
+//class- Blueprint
+
+// class Myclass{
+//     constructor(){
+
+//     }
+//     myMethod(){}
+// }
+
+// let myObj = new Myclass(); syntax of an class
+
+
+
+// class car{
+
+//     constructor(){
+//         console.log('creating new object');
+//     }
+//     start(){
+//         console.log('start');
+//     }
+
+//     stop(){
+//         console.log('stop');
+//     }
+
+//     setBrand(brand){
+//         this.brandName = brand;
+//     }
+// }
+
+// let mercedes = new car();
+// mercedes.setBrand('fortuner')
+
+
+// class parent{
+//     hello(){
+//         console.log('hello');
+//     }
+// }
+
+// class child extends parent {
+
+// }
+
+// let Obj = new child();
+
+
+
