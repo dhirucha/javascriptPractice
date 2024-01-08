2a1e205c09a3868e4a955d9fea2a4cb979809a2c
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-01-08 21:55:15 +0530

    learned promise

--- class_11/app.js
+++ class_11/app.js
@@ -1,13 +1,27 @@
 
 //Callback e.g.
-function sum(a,b){
-    console.log(a + b);
-}
+// function sum(a,b){
+//     console.log(a + b);
+// }
 
-function calculator(a,b,sumCalback){
-    sumCalback(a,b)
-}
+// function calculator(a,b,sumCallback){
+//     sumCallback(a,b)
+// }
 
-calculator(1,2,sum)
+// calculator(1,2,sum)
 
+
+//promises
+
+let promise = new Promise((resolve,reject) => {
+    console.log('I am a promise');
+    resolve('success');
+    //reject('some error occured')
+})
+
+
+let promises = promise();
+promises.then(() => {
+    console.log('promise fulfilled');
+})
 
